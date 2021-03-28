a0cfffd72b8cf5a3fa2be9066803f7978dff9a2c
Johannes Berthel <j.berthel@codeblick.de>
2021-03-28 23:09:16 +0200

    updated service and dependencies

--- src/errors.js
+++ src/errors.js
@@ -4,27 +4,26 @@
  * MIT Licensed
  */
 
-
-const { MoleculerError } = require("moleculer").Errors;
-const ERR_INVALID_TOKEN = "ERR_INVALID_TOKEN";
+const { MoleculerError } = require('moleculer').Errors;
+const ERR_INVALID_TOKEN = 'ERR_INVALID_TOKEN';
 class UnAuthorizedError extends MoleculerError {
-	/**
-	 * Creates an instance of UnAuthorizedError.
-	 *
-	 * @param {String} type
-	 * @param {any} data
-	 *
-	 * @memberOf UnAuthorizedError
-	 */
-	constructor(type, data) {
-		super("Unauthorized", 401, type || ERR_INVALID_TOKEN, data);
-	}
+    /**
+     * Creates an instance of UnAuthorizedError.
+     *
+     * @param {String} type
+     * @param {any} data
+     *
+     * @memberOf UnAuthorizedError
+     */
+    constructor(type, data) {
+        super('Unauthorized', 401, type || ERR_INVALID_TOKEN, data);
+    }
 }
 
 class BadRequestError extends MoleculerError {
-	constructor(type, data){
-		super('Bad Request', 400, type || "ERR_INVALID_FORMAT", data)
-	}
+    constructor(type, data) {
+        super('Bad Request', 400, type || 'ERR_INVALID_FORMAT', data);
+    }
 }
 
-module.exports = {UnAuthorizedError, BadRequestError}
+module.exports = { UnAuthorizedError, BadRequestError };
